05fe10581c5265ed73ff28d46f223348fea24668
Kurt Heiritz (pseudo) <saarthkarkera@gmail.com>
2025-05-28 08:55:32 +0530

    Use colored instead of termcolor

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,20 @@
 use clap::{Parser, Subcommand};
+use colored::Colorize;
 use rusqlite::{params, Connection};
 use rusqlite::types::Null;
 use std::io::Write;
-use termcolor::{Color, ColorChoice, ColorSpec, StandardStream, WriteColor};
 use time::{PrimitiveDateTime, OffsetDateTime, Time, Duration};
+
+fn slice_to_string(slice: &[i32]) -> String {
+    let mut str = String::new();
+    for (i, item) in slice.iter().enumerate() {
+        str.push_str(item.to_string().as_str());
+        if i <= slice.len() - 2 {
+            str.push_str(", ");
+        }
+    }
+    str
+}
 
 #[derive(Parser)]
 #[command(version, about, long_about = None)]
@@ -50,10 +61,6 @@ fn read_line() -> String {
 fn list_items(conn: &Connection) -> Result<(), Box<dyn std::error::Error>> {
     let mut stmt = conn.prepare("SELECT * FROM items")?;
     let rows = stmt.query_map((), |rows| Ok((rows.get::<_, i32>(0)?, rows.get::<_, String>(1)?, rows.get::<_, PrimitiveDateTime>(2)?, rows.get::<_, bool>(3)?)))?;
-    let mut colored_red_stdout = StandardStream::stdout(ColorChoice::Always);
-    let mut colored_yellow_stdout = StandardStream::stdout(ColorChoice::Always);
-    colored_red_stdout.set_color(ColorSpec::new().set_fg(Some(Color::Red)))?;
-    colored_yellow_stdout.set_color(ColorSpec::new().set_fg(Some(Color::Yellow)))?;
     println!("{0: <10} | {1: <50} | {2: <20} | {3: <10}", "ID", "Description", "Due Date", "Status");
     let row_vec = rows.map(|rows| rows.unwrap()).collect::<Vec<_>>();
     let now = PrimitiveDateTime::new(OffsetDateTime::now_local()?.date(), OffsetDateTime::now_local()?.time());
@@ -65,9 +72,9 @@ fn list_items(conn: &Connection) -> Result<(), Box<dyn std::error::Error>> {
         let is_completed = row.3;
         if !is_completed {
             if now < due_date && due_date < now + Duration::days(3) {
-                writeln!(colored_yellow_stdout, "{0: <10} | {1: <50} | {2: <20} | {3: <10}", id, description, due_date, is_completed)?;
+                println!("{0: <10} | {1: <50} | {2: <20} | {3: <10}", id.to_string().yellow(), description.yellow(), due_date.to_string().yellow(), is_completed.to_string().yellow());
             } else if PrimitiveDateTime::new(OffsetDateTime::now_local()?.date(), OffsetDateTime::now_local()?.time()) > due_date {
-                writeln!(colored_red_stdout, "{0: <10} | {1: <50} | {2: <20} | {3: <10}", id, description, due_date, is_completed)?;
+                println!("{0: <10} | {1: <50} | {2: <20} | {3: <10}", id.to_string().red(), description.red(), due_date.to_string().red(), is_completed.to_string().red());
             } else {
                 println!("{0: <10} | {1: <50} | {2: <20} | {3: <10}", id, description, due_date, is_completed);
             }
@@ -75,8 +82,8 @@ fn list_items(conn: &Connection) -> Result<(), Box<dyn std::error::Error>> {
     };
     let overdue_items = &row_vec.iter().filter(|row| row.2 < now).map(|row| row.0).collect::<Vec<_>>()[..];
     let close_items = &row_vec.iter().filter(|row| now + Duration::days(3) > row.2 && row.2 > now).map(|row| row.0).collect::<Vec<_>>()[..];
-    if overdue_items.len() > 0 { writeln!(colored_red_stdout, "Items {:?} are overdue! Finish them quickly!", overdue_items)?; }
-    if close_items.len() > 0 { writeln!(colored_yellow_stdout, "Items {:?} are close to deadline, you should finish them soon!", close_items)?; }
+    if overdue_items.len() > 0 { println!(, "Items {:?} are overdue! Finish them quickly!", overdue_items); }
+    if close_items.len() > 0 { println!("{} {:?} {}", "Items".yellow(), slice_to_string(close_items).yellow(), "are close to deadline! Finish them quickly!".yellow()); }
     Ok(())
 }
 
